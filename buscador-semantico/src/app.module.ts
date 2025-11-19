e3ea686fcc5402b67baece4af5468bf62a59cdd8
GuiladVillarroel <guilad1233@gmail.com>
2025-11-19 07:38:24 -0400

    feat: integrate Cloudinary for file uploads and manage OWL documents

--- buscador-semantico/src/app.module.ts
+++ buscador-semantico/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { OwlModule } from './modules/owl/owl.module';
+import { UploadModule } from './modules/upload/upload.module';
 
 @Module({
-  imports: [PrismaModule, OwlModule],
+  imports: [PrismaModule, OwlModule, UploadModule],
   controllers: [AppController],
   providers: [AppService],
 })
